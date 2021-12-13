0b0228c6c10aee4223e4bd36cf2fa374b0e49cdd
Aaroncea <92772873+Aaroncea@users.noreply.github.com>
2021-12-13 12:46:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("info").innerHTML = "I am a God-fearing man who also loves playing basketball and Videogames. Another thing I love is travelling to different places.";
+}
+function myfunction() {
+	document.getElementById("demo").style.display = "block";
+}
+function myfunction() {
+	document.getElementById("demo2").style.display = "block";
+}
